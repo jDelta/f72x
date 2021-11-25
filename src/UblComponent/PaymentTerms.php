8663f942dd607268900767fce2eb9112e66915e3
Jaime Cruz <jaimecruz.dev@gmail.com>
2021-11-25 17:34:51 -0500

    Actualización SUNAT forma de pago (Contado/Credito)

---
+++ src/UblComponent/PaymentTerms.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * MÓDULO DE EMISIÓN ELECTRÓNICA F72X
+ * UBL 2.1
+ * Version 1.0
+ * 
+ * Copyright 2019, Jaime Cruz
+ */
+
+namespace F72X\UblComponent;
+
+use Sabre\Xml\Writer;
+use Sabre\Xml\Element\Cdata;
+
+class PartyName extends BaseComponent {
+
+    protected $Name;
+
+    function xmlSerialize(Writer $writer) {
+        $writer->write([
+            SchemaNS::CBC . 'Name' => new Cdata($this->Name)
+        ]);
+    }
+
+    public function getName() {
+        return $this->Name;
+    }
+
+    public function setName($Name) {
+        $this->Name = $Name;
+        return $this;
+    }
+
+}
