b58ef2856326c12dce5ee79fe9f92904913662ff
Jaime Cruz <jaimecruz.dev@gmail.com>
2024-07-14 13:13:53 -0500

    fix: SunatGatewayTest unauthorized error

--- tests/SunatGatewayTest.php
+++ tests/SunatGatewayTest.php
@@ -7,10 +7,19 @@ use F72X\Sunat\ServiceGateway;
 
 final class SunatGatewayTest extends TestCase
 {
+    // Algunas veces el servicio de SUNAT puede demorar en responder
+    // por lo que se ha definido un tiempo de espera de 2 segundos
+    // Para ejecutar las siguientes pruebas sin problemas
+    // se ha definido un tiempo de espera de 2 segundos
+    private $sleepTime = 1;
     private static ServiceGateway $sunatGateway;
     public static function setUpBeforeClass(): void
     {
         self::$sunatGateway = new ServiceGateway();
+    }
+    private function someSleep()
+    {
+        sleep($this->sleepTime);
     }
     public function testSendBoletaCase1()
     {
@@ -18,6 +27,7 @@ final class SunatGatewayTest extends TestCase
             'responseCode' => '0',
             'responseDesc' => 'La Boleta numero B001-00003652, ha sido aceptada'
         ];
+        $this->someSleep();
         $response = self::$sunatGateway->sendBill('20100454523-03-B001-00003652');
         $actual = [
             'responseCode' => $response['responseCode'],
@@ -32,6 +42,7 @@ final class SunatGatewayTest extends TestCase
             'responseCode' => '0',
             'responseDesc' => 'La Nota de Credito numero FC01-00000211, ha sido aceptada'
         ];
+        $this->someSleep();
         $response = self::$sunatGateway->sendBill('20100454523-07-FC01-00000211');
         $actual = [
             'responseCode' => $response['responseCode'],
@@ -46,6 +57,7 @@ final class SunatGatewayTest extends TestCase
             'responseCode' => '0',
             'responseDesc' => 'La Nota de Debito numero FD01-00000211, ha sido aceptada'
         ];
+        $this->someSleep();
         $response = self::$sunatGateway->sendBill('20100454523-08-FD01-00000211');
         $actual = [
             'responseCode' => $response['responseCode'],
@@ -57,6 +69,7 @@ final class SunatGatewayTest extends TestCase
     public function testSendResumenDiario()
     {
         $this->expectNotToPerformAssertions();
+        $this->someSleep();
         $ticket = self::$sunatGateway->sendSummary('20100454523-RC-20171118-00001');
         echo $ticket;
     }
@@ -79,6 +92,7 @@ final class SunatGatewayTest extends TestCase
     public function testGetResumenDiarioStatus()
     {
         $this->expectNotToPerformAssertions();
+        $this->someSleep();
         $response = self::$sunatGateway->getStatus('20100454523-RC-20171118-00001');
         echo json_encode($response);
     }
